eaee93a9213552f9856902b5f8024d72b2207aae
Khánh Pluto <47073675+khanh41@users.noreply.github.com>
2021-11-11 15:39:34 +0700

    Update mongodb model and user login, add super user and env  (#12)
    * Update mongodb model and remove file not need
    
    * Update Dockerfile with cron and deploy project
    
    * Update template.env
    
    * Update user login, add super user and env

--- {{cookiecutter.project_slug}}/app/api/routes/authentication.py
+++ {{cookiecutter.project_slug}}/app/api/routes/authentication.py
@@ -88,8 +88,3 @@ async def get_documentation(current_user: UserSchema = Depends(authentication_se
 @router.get("/users/me/", response_model=UserSchema)
 async def read_users_me(current_user: UserSchema = Depends(authentication_service.get_current_active_user)):
     return current_user
-
-
-@router.get("/users/me/items/")
-async def read_own_items(current_user: UserSchema = Depends(authentication_service.get_current_active_user)):
-    return [{"item_id": "Foo", "owner": current_user.username}]
