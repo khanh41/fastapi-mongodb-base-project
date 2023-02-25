0edd75080640b0d6c16523dfbd771f83914bed90
Khánh Pluto <tranvankhanh99a@gmail.com>
2023-02-25 23:27:29 +0700

    Format source code with flake8

--- {{cookiecutter.project_slug}}/app/api/database/migrate/init_super_user.py
+++ {{cookiecutter.project_slug}}/app/api/database/migrate/init_super_user.py
@@ -1,18 +1,19 @@
+"""Init superuser."""
 from fastapi.encoders import jsonable_encoder
 
 from app.api.database.execute.user import user_execute as execute
 from app.api.services import authentication_service
-from app.core.constant import ADMIN_USER, SUPER_USERNAME, SUPER_PASSWORD
+from app.core.constant import ADMIN_USER, SUPER_PASSWORD, SUPER_USERNAME
 
 
 def init_super_user():
+    """Init superuser if not exist."""
     if not execute.retrieve_data_by_role(ADMIN_USER):
         print("Create Super user...")
-        user = {
+        user = jsonable_encoder({
             "username": str(SUPER_USERNAME),
             "password": str(SUPER_PASSWORD),
             "role": -1,
-        }
-        user = jsonable_encoder(user)
+        })
         user['password'] = authentication_service.get_password_hash(user['password'])
         execute.add_data(user)
--- {{cookiecutter.project_slug}}/app/main.py
+++ {{cookiecutter.project_slug}}/app/main.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI
-from fastapi import Request
+"""Config and run application."""
+import os
+
+import uvicorn
+from fastapi import FastAPI, Request
 from fastapi.exceptions import RequestValidationError
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -11,17 +14,21 @@ from app.api.errors.http_error import http_error_handler
 from app.api.errors.validation_error import http422_error_handler
 from app.core.config import ALLOWED_HOSTS, API_PREFIX, DEBUG, PROJECT_NAME, VERSION
 
+if PROJECT_NAME:
+    from app.api.database.migrate.init_super_user import init_super_user
+    from app.api.routes import authentication
+    from app.api.routes.api import app as api_router
+else:
+    raise ImportError("Wrong import order")
+
 
 def get_application() -> FastAPI:
-    from app.api.routes.api import app as api_router
-    from app.api.routes import authentication
-    from app.api.helpers.download import download_video_template
-    from app.api.database.migrate.init_super_user import init_super_user
-
-    download_video_template()
+    """Define application config."""
     init_super_user()
 
-    application = FastAPI(title=PROJECT_NAME, debug=DEBUG, version=VERSION, docs_url=None)
+    application = FastAPI(
+        title=PROJECT_NAME, debug=DEBUG, version=VERSION, docs_url=None
+    )
     print()
     application.add_middleware(
         CORSMiddleware,
@@ -30,9 +37,6 @@ def get_application() -> FastAPI:
         allow_methods=["*"],
         allow_headers=["*"],
     )
-
-    # application.add_event_handler("startup", create_start_app_handler(application))
-    # application.add_event_handler("shutdown", create_stop_app_handler(application))
 
     application.add_exception_handler(HTTPException, http_error_handler)
     application.add_exception_handler(RequestValidationError, http422_error_handler)
@@ -48,6 +52,7 @@ def get_application() -> FastAPI:
 
     @application.get("/", tags=["UI"], response_class=HTMLResponse, deprecated=False)
     async def read_root(request: Request):
+        """Template Response UI."""
         return templates.TemplateResponse("index.html", {"request": request})
 
     return application
@@ -56,9 +61,6 @@ def get_application() -> FastAPI:
 app = get_application()
 
 if __name__ == "__main__":
-    import uvicorn
-    import os
-
     HOST = os.getenv("APP_HOST")
     PORT = os.getenv("APP_PORT")
     uvicorn.run(app, host=HOST, port=int(PORT))
