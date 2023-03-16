d2b84a73175fa5af42f73a33d70f9c866cbd04d3
Khánh Pluto <tranvankhanh99a@gmail.com>
2023-03-16 16:56:25 +0700

    Convert output of db to model

--- {{cookiecutter.project_slug}}/app/api/database/execute/user.py
+++ {{cookiecutter.project_slug}}/app/api/database/execute/user.py
@@ -3,26 +3,26 @@ from __future__ import annotations
 
 from app.api.database.connect import user_collection
 from app.api.database.execute.base_execute import BaseExecute
-from app.api.helpers.convert_model2dict import user_helper
+from app.api.database.models.user import UserSchema
+from app.api.helpers.dict2model import convert_user_to_model
 
 
 class UserExecute(BaseExecute):
     """User Execute."""
 
-    # Retrieve a data with a matching email
-    def retrieve_data_by_username(self, username: str) -> dict | None:
+    def retrieve_data_by_username(self, username: str) -> UserSchema | None:
         """Get user information by username."""
-        data = self.data_collection.find_one({"username": username})
+        data: dict = self.data_collection.find_one({"username": username})
         if data:
-            return self.data_helper(data)
+            return self.convert_helper(data)
         return None
 
-    def retrieve_data_by_role(self, role: int) -> dict | None:
+    def retrieve_data_by_role(self, role: int) -> UserSchema | None:
         """Get user information by role."""
-        data = self.data_collection.find_one({"role": role})
+        data: dict = self.data_collection.find_one({"role": role})
         if data:
-            return self.data_helper(data)
+            return self.convert_helper(data)
         return None
 
 
-user_execute = UserExecute(user_collection, user_helper)
+user_execute = UserExecute(user_collection, convert_user_to_model)
--- {{cookiecutter.project_slug}}/app/api/database/models/user.py
+++ {{cookiecutter.project_slug}}/app/api/database/models/user.py
@@ -1,29 +1,21 @@
 # pylint: skip-file
 """User Model."""
-
+from datetime import datetime
 from typing import Optional
 
-from fastapi import HTTPException
-from fastapi.openapi.models import (
-    OAuthFlows as OAuthFlowsModel,
-    SecurityBase as SecurityBaseModel,
-)
-from fastapi.security import OAuth2
-from fastapi.security.base import SecurityBase
-from fastapi.security.utils import get_authorization_scheme_param
-from passlib.context import CryptContext
-from pydantic import BaseModel, Field
-from starlette.requests import Request
-from starlette.status import HTTP_403_FORBIDDEN
+from pydantic import Field
+
+from app.api.database.models.base import CustomBaseModel
 
 
-class UserSchema(BaseModel):
+class UserSchema(CustomBaseModel):
     """User Schema."""
+    userID: Optional[str] = None
     username: str = Field(...)
     password: str = Field(...)
     role: int = Field(...)
-    createAt: Optional[str] = None
-    updateAt: Optional[str] = None
+    createAt: Optional[datetime] = None
+    updateAt: Optional[datetime] = None
 
     class Config:
         """Config."""
@@ -34,96 +26,3 @@ class UserSchema(BaseModel):
                 "role": 0,
             }
         }
-
-
-class Token(BaseModel):
-    """Token."""
-    access_token: str
-    token_type: str
-
-
-class TokenData(BaseModel):
-    """Token Data."""
-    username: Optional[str] = None
-
-
-class UserInDB(UserSchema):
-    """User In DB"""
-    hashed_password: str
-
-
-class OAuth2PasswordBearerCookie(OAuth2):
-    """Authentication Bearer Cookie."""
-
-    def __init__(
-            self,
-            tokenUrl: str,
-            scheme_name: str = None,
-            scopes: dict = None,
-            auto_error: bool = True,
-    ) -> None:
-        if not scopes:
-            scopes = {}
-        flows = OAuthFlowsModel(password={"tokenUrl": tokenUrl, "scopes": scopes})
-        super().__init__(flows=flows, scheme_name=scheme_name, auto_error=auto_error)
-
-    async def __call__(self, request: Request) -> Optional[str]:
-        header_authorization: str = request.headers.get("Authorization")
-        cookie_authorization: str = request.cookies.get("Authorization")
-
-        header_scheme, header_param = get_authorization_scheme_param(
-            header_authorization
-        )
-        cookie_scheme, cookie_param = get_authorization_scheme_param(
-            cookie_authorization
-        )
-
-        if header_scheme.lower() == "bearer":
-            authorization = True
-            scheme = header_scheme
-            param = header_param
-
-        elif cookie_scheme.lower() == "bearer":
-            authorization = True
-            scheme = cookie_scheme
-            param = cookie_param
-
-        else:
-            authorization = False
-            scheme = ""
-            param = ""
-
-        if not authorization or scheme.lower() != "bearer":
-            if self.auto_error:
-                raise HTTPException(
-                    status_code=HTTP_403_FORBIDDEN, detail="Not authenticated"
-                )
-            return None
-        return param
-
-
-class BasicAuth(SecurityBase):
-    """Basic Auth."""
-
-    def __init__(self, scheme_name: str = None, auto_error: bool = True):
-        self.model = SecurityBaseModel(type="http")
-        self.scheme_name = scheme_name or self.__class__.__name__
-        self.auto_error = auto_error
-
-    async def __call__(self, request: Request) -> Optional[str]:
-        authorization: str = request.headers.get("Authorization")
-        scheme, param = get_authorization_scheme_param(authorization)
-        if not authorization or scheme.lower() != "basic":
-            if self.auto_error:
-                raise HTTPException(
-                    status_code=HTTP_403_FORBIDDEN, detail="Not authenticated"
-                )
-            return None
-        return param
-
-
-basic_auth = BasicAuth(auto_error=False)
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-oauth2_scheme = OAuth2PasswordBearerCookie(tokenUrl="/token")
