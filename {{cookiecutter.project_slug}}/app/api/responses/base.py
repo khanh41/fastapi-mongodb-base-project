6f05800e68efae7ba024353d4b67dc24c311d66e
Khánh Pluto <47073675+khanh41@users.noreply.github.com>
2023-05-31 13:44:30 +0700

    🐣 Change `JSONResponse` to `ORJSONResponse` (#35)
    Co-authored-by: KhanhTV <khanhtv@nal.vn>

--- {{cookiecutter.project_slug}}/app/api/responses/base.py
+++ {{cookiecutter.project_slug}}/app/api/responses/base.py
@@ -1,39 +1,47 @@
 """Base Response."""
 
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, ORJSONResponse
 from starlette import status
 
 
 class BaseResponse:
     """Base Response."""
 
-    def __init__(self) -> None:
-        """Init base response."""
-        self.base_response = {
-            "message_code": 0,
-            "message": "",
-            "data": None,
-        }
-
     @staticmethod
-    def success_response(message: str = "API success", status_code: int = status.HTTP_200_OK, message_code: int = 0):
+    def success_response(
+            message: str = "API success",
+            status_code: int = status.HTTP_200_OK,
+            message_code: int = 0,
+            data=None,
+    ):
         """Success response with message and status code.
 
         Args:
             message: API Message.
             status_code: API status code.
             message_code: API message code.
+            data: Response data.
 
         Returns:
             Success response.
         """
-        return JSONResponse(
-            status_code=status_code,
-            content={
-                "message_code": message_code,
-                "message": message,
-            }
-        )
+        if data is None:
+            return JSONResponse(
+                status_code=status_code,
+                content={
+                    "message_code": message_code,
+                    "message": message,
+                },
+            )
+        else:
+            return ORJSONResponse(
+                status_code=status_code,
+                content={
+                    "message_code": message_code,
+                    "message": message,
+                    "data": data,
+                },
+            )
 
     @staticmethod
     def error_response(message: str = "API error", status_code: int = status.HTTP_200_OK, message_code: int = 0):
@@ -54,6 +62,3 @@ class BaseResponse:
                 "message": message,
             }
         )
-
-
-response = BaseResponse()
